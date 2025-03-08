dfe992847f4f34d34694865c5e7d3ba2d7d73140
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-08 17:20:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const makeChange = (c) => {
-  // your name here
+  // your name her
+	let c = {
+        q: Math.floor(amount / 25),  // Number of quarters
+        d: Math.floor((amount % 25) / 10),  // Remaining amount converted to dimes
+        n: Math.floor((amount % 10) / 5),  // Remaining amount converted to nickels
+        p: amount % 5  // Remaining amount as pennies
+    };
+
+    return c;
 };
 
 // Do not the change the code below
