f95eb8014d46fb53f42022df0281d66c4a89333b
shivrajsingh123 <45819719+shivrajsingh123@users.noreply.github.com>
2025-03-08 17:48:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const makeChange = (c) => {
   // your name her
-	 c = parseInt(c); // Ensure input is treated as a number
+	c = parseInt(c); // Ensure input is treated as a number
 
-  return {
-    q: Math.floor(c / 25), // Number of quarters
-    d: Math.floor((c % 25) / 10), // Number of dimes
-    n: Math.floor((c % 10) / 5), // Number of nickels
-    p: c % 5 // Number of pennies
+  let q = Math.floor(c / 25); // Number of quarters
+  let d = Math.floor((c % 25) / 10); // Number of dimes
+  let remaining = c % 25 % 10; // Remaining cents after quarters & dimes
+  
+  let n = Math.floor(remaining / 5); // Number of nickels
+  let p = remaining % 5; // Number of pennies
+
+  return { q, d, n, p };
   };
 };
 
